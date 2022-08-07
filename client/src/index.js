e4df0d5a59bc4eef018c25de67e7aac4475d7e03
Dimas8889 <105507458+Dimas8889@users.noreply.github.com>
2022-08-07 18:54:14 +0300

    add material ui

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,39 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {ThemeProvider,createTheme} from '@material-ui/core';
+
+const theme = createTheme({
+    pallete: {
+        primary: {
+            light: '#FFFFFF',
+            main: '#847A7A',
+            dark: '#000000'
+
+        },
+        secondary: {
+            light: '#E2DFDF',
+            main: '#847A7A',
+            dark: '#595959'
+
+        }
+    },
+    typography: {
+        fontFamily: [
+            'Josefin Sans',
+            'Open Sans',
+        ].join(','),
+    }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <ThemeProvider theme={theme}>
     <BrowserRouter>
       <App />
       </BrowserRouter>
+      </ThemeProvider>
   </React.StrictMode>
 );
 
