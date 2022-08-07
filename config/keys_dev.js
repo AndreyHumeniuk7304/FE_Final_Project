0b7ad6f760bfdad9313a61bc2f9fde364d807df8
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2022-08-07 18:21:26 +0300

    Update gitignore add .env and connect DB

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'Сюда копируем URI для доступа в БД, например: mongodb+srv://username:password@..........................',
-  secretOrKey: 'random very very secret string',
+    'mongodb+srv://admin:K5NywjzWadHoIHNd@cluster0.u5hxfin.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'somesecret',
 };
