1e72c5afcd6afe475ddf91fa483e23d60a112fad
Andrey <andreyhumeniuk99@gmail.com>
2022-08-09 15:43:23 +0300

    started header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,37 +3,63 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__categories-container">
-        <Link to="/man" className="">
-          MAN
-        </Link>
-        <Link to="/woman" className="">
-          WOMEN
-        </Link>
-        <Link to="/accessory" className="">
-          ACCESSORY
-        </Link>
-      </div>
+      <div className="container">
+        <div className="header_top-content">
+          <div className="header__logo-container">
+            <Link to="/" className="header__logo-container_link">
+              <img src="./images/header-logo.png" alt="logo" />
+            </Link>
+          </div>
 
-      <div className="header__logo-container">
-        <Link to="/" className="">
-        Originalité
-        </Link>
-      </div>
+          <div className="header__account-container">
+            <Link to="/search" className="header__account-container__icon">
+              <img
+                className="header__account-container__icon-img"
+                src="./images/search-ico.svg"
+                alt="search"
+              />
+            </Link>
+            <Link to="/my-account" className="header__account-container__icon">
+              <img
+                className="header__account-container__icon-img"
+                src="./images/account-ico.svg"
+                alt="my-account"
+              />
+            </Link>
+            <Link
+              to="/shopping-bag"
+              className="header__account-container__icon"
+            >
+              <img
+                className="header__account-container__icon-img"
+                src="./images/shopping-bag-ico.svg"
+                alt="my-account"
+              />
+            </Link>
 
-      <div className="header__account-container">
-        <Link to="/search" className="">
-          Search
-        </Link>
-        <Link to="/my-account" className="">
-          My account
-        </Link>
-        <Link to="/shopping-bag" className="">
-          Shopping bag
-        </Link>
+            <div className="header__account-container__icon">
+              <img
+                className="header__account-container__icon-img"
+                src="./images/burger-ico.svg"
+                alt="my-account"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="header__categories-container">
+          <Link to="/man" className="">
+            MAN
+          </Link>
+          <Link to="/woman" className="">
+            WOMEN
+          </Link>
+          <Link to="/accessory" className="">
+            ACCESSORY
+          </Link>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
