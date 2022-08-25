ec85f76c506e67633ae400805d21a04845e0835e
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-08-25 10:56:25 +0300

    input data and yup in login-regist to data.js

--- client/src/components/Entry/Login.jsx
+++ client/src/components/Entry/Login.jsx
@@ -5,42 +5,19 @@ import { fetchProducts } from "../../store/userAccount/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import Form from "../Forms/Form";
-import ProductForm from "../ProductForm/ProductForm";
-
-const loginInputNames = [
-  { inputName: "loginOrEmail", formType: "input" },
-  { inputName: "password", formType: "password" },
-  {
-    inputName: "isSignedAutomatically",
-    formType: "checkbox",
-    label: "Keep me signed in",
-    className: "entry__checkbox",
-  },
-];
+import { loginInputNames, loginSchema } from "./data";
 
 const Login = () => {
   const dispatch = useDispatch();
   const nav = useNavigate();
   const error = useSelector((state) => state.userAccount.error);
 
-  const schema = yup.object({
-    loginOrEmail: yup
-      .string()
-      .required("login/Email is required.")
-      .min(5, "Login is too short - should be 5 chars minimum."),
-    password: yup
-      .string()
-      .required("Password is required.")
-      .max(30, "Password is too long - should be 30 chars maximum.")
-      .min(7, "Password is too short - should be 7 chars minimum."),
-  });
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(loginSchema),
     defaultValues: {
       loginOrEmail: "",
       password: "",
@@ -64,7 +41,7 @@ const Login = () => {
         formArr={loginInputNames}
         register={register}
         handleSubmit={handleSubmit}
-        errors={errors}
+        errors={errors || error}
         btnName={"LOG IN"}
       />
     </>
--- client/src/components/Entry/Registrations.jsx
+++ client/src/components/Entry/Registrations.jsx
@@ -1,51 +1,18 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
 import "yup-phone";
 import addNewCustomers from "../../api/addNewCustomer";
 import PropTypes from "prop-types";
 import Form from "../Forms/Form";
-
-const registInputNames = [
-  { inputName: "firstName", formType: "input" },
-  { inputName: "lastName", formType: "input" },
-  { inputName: "login", formType: "input" },
-  { inputName: "email", formType: "input" },
-  { inputName: "telephone", formType: "input" },
-  { inputName: "gender", formName: ["Male", "Female"], formType: "droplist" },
-  { inputName: "password", formType: "password" },
-  { inputName: "confirmPassword", formType: "password" },
-];
+import { registInputNames, registSchema } from "./data";
 
 const Registrations = ({ setIsRegist }) => {
-  const schema = yup.object({
-    firstName: yup.string().required("First name is required."),
-    lastName: yup.string().required("Second name is required."),
-    login: yup
-      .string()
-      .required("Login is required.")
-      .min(5, "Login is too short - should be 5 chars minimum."),
-    email: yup.string().required("Email is required.").email("Email not valid"),
-    telephone: yup.string().required("Telephone is required.").phone(),
-    password: yup
-      .string()
-      .required("Password is required.")
-      .max(30, "Password is too long - should be 30 chars maximum.")
-      .min(7, "Password is too short - should be 7 chars minimum."),
-    confirmPassword: yup
-      .string()
-      .required("Password is required.")
-      .max(30, "Password is too long - should be 30 chars maximum.")
-      .min(7, "Password is too short - should be 7 chars minimum.")
-      .oneOf([yup.ref("password"), null], "Passwords must match"),
-  });
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(registSchema),
     defaultValues: {
       firstName: "",
       lastName: "",
