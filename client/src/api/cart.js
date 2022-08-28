d584202522abe4a65b59bcb8addc198ba2f2150a
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2022-08-28 14:23:35 +0300

    fix crash after no authorization request

--- client/src/api/cart.js
+++ client/src/api/cart.js
@@ -1,64 +1,36 @@
 import axios from "../ulits/instance/instance";
 
 export const createCart = async (products) => {
-  try {
-    const { data } = await axios.post("/cart", { products });
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.post("/cart", { products });
+  return data;
 };
 
 export const updateCart = async (products) => {
-  try {
-    const { data } = await axios.put("/cart", { products });
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.put("/cart", { products });
+  return data;
 };
 
 export const addProductToCart = async (productID) => {
-  try {
-    const { data } = await axios.put(`/cart/${productID}`);
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.put(`/cart/${productID}`);
+  return data;
 };
 
 export const decreaseQuantity = async (productID) => {
-  try {
-    const { data } = await axios.delete(`/cart/product/${productID}`);
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.delete(`/cart/product/${productID}`);
+  return data;
 };
 
 export const deleteProduct = async (productID) => {
-  try {
-    const { data } = await axios.delete(`/cart/${productID}`);
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.delete(`/cart/${productID}`);
+  return data;
 };
 
 export const getCart = async () => {
-  try {
-    const { data } = await axios.get("/cart");
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.get("/cart");
+  return data;
 };
 
 export const deleteCart = async () => {
-  try {
-    const { data } = await axios.delete("/cart");
-    return data;
-  } catch (err) {
-    return err;
-  }
+  const { data } = await axios.delete("/cart");
+  return data;
 };
