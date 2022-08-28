8bb04c4a41d47216abe3a0a69195ed50cea98d89
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-08-28 23:14:36 +0300

    change getCustomer.js

--- client/src/api/getCustomers.js
+++ client/src/api/getCustomers.js
@@ -1,15 +1,4 @@
-import axiosDefault from "axios";
-
-const axios = axiosDefault.create({
-  baseURL: "http://localhost:3001/api",
-});
-
-export const setAuthToken = (token) => {
-  token
-    ? (axios.defaults.headers.common["Authorization"] = token)
-    : delete axios.defaults.headers.common["Authorization"];
-  return axios.defaults.headers.common["Authorization"];
-};
+import axios from "../ulits/instance/instance";
 
 export default async function getCustomers(userData) {
   return axios.post("/customers/login", userData);
