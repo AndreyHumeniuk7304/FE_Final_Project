0454b3e0a6d7571fdf7c4c6004ac677ac2f2f04d
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-09-07 14:33:11 +0300

    remove btn show more

--- client/src/components/Catalog/ProductList/Pagination/Pagination.jsx
+++ client/src/components/Catalog/ProductList/Pagination/Pagination.jsx
@@ -1,58 +1,29 @@
-import { Button, Pagination } from "@mui/material";
-import { useEffect } from "react";
-import { useState } from "react";
+import { Pagination } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 
 const Paginations = () => {
   const [search, setSearch] = useSearchParams();
-  const [numOfProductsOnPage, setNumOfProductsOnPage] = useState(
-    +search.getAll("perPage")
-  );
-  const [currentPage, setCurrentPage] = useState(+search.getAll("startPage"));
-  const [pageCount, setPageCount] = useState();
   const productsQuntity = useSelector((state) => state.catalog.productsQuntity);
-
-  useEffect(() => {
-    setPage();
-  }, [numOfProductsOnPage, currentPage]);
-
-  useEffect(() => {
-    setPageCount(Math.ceil(productsQuntity / +search.getAll("perPage")));
-  }, []);
-
-  const setPage = () => {
+  const setPage = (currentPage) => {
     const linkWithoutPagesInfo = search
       .toString()
       .slice(0, search.toString().search("perPage"));
 
-    setSearch(
-      linkWithoutPagesInfo +
-        `perPage=${numOfProductsOnPage}&startPage=${currentPage}`
-    );
+    setSearch(linkWithoutPagesInfo + `perPage=10&startPage=${currentPage}`);
   };
 
   return (
-    pageCount !== 1 && (
-      <>
-        {productsQuntity > numOfProductsOnPage &&
-          +search.getAll("startPage") === 1 && (
-            <Button
-              onClick={() => setNumOfProductsOnPage(numOfProductsOnPage + 10)}
-            >
-              Show more
-            </Button>
-          )}
-        <Pagination
-          shape="rounded"
-          onChange={(e, v) => {
-            setCurrentPage(v);
-          }}
-          page={+search.getAll("startPage")}
-          count={pageCount}
-        />
-      </>
-    )
+    <>
+      <Pagination
+        shape="rounded"
+        onChange={(e, v) => {
+          setPage(v);
+        }}
+        page={+search.getAll("startPage")}
+        count={Math.ceil(productsQuntity / 10)}
+      />
+    </>
   );
 };
 
