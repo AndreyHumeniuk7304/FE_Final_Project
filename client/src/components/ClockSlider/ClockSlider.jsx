cf8cf0fbd7be0f3a3579d74a2a84c02c895593d8
Bogdan <litvinenkobv99@gmail.com>
2022-09-13 22:16:25 +0300

    code review - slider, popular and new products

--- client/src/components/ClockSlider/ClockSlider.jsx
+++ client/src/components/ClockSlider/ClockSlider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {
   Autoplay,
@@ -9,43 +9,46 @@ import SwiperCore, {
 import "swiper/css";
 import "swiper/css/pagination";
 import { Link } from "react-router-dom";
+import { getSliders } from "../../api/slider";
 
 const ClockSlider = () => {
+  const [sliders, setSliders] = useState([]);
+
+  useEffect(() => {
+    getSliders().then((res) => setSliders(res));
+  }, []);
+
   return (
-    <Swiper
-      className="swiper"
-      modules={[Navigation, Pagination, Scrollbar]}
-      effect="slider"
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: true,
-      }}
-      spaceBetween={0}
-      slidesPerView={1}
-      loop={true}
-      navigation={true}
-      speed={1500}
-      pagination={{ clickable: true }}
-    >
-      <SwiperSlide className="swiper__slider" onClick={() => console.log()}>
-        <Link to="/" className="">
-          <div className="swiper__wrapper"></div>
-          <img src="https://res.cloudinary.com/fe-advjs-final-project-3/image/upload/v1660230409/Garmin_Sale_Desktop_jd7g1t.webp" />
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="swiper__slider" onClick={() => console.log()}>
-        <Link to="/" className="">
-          <div className="swiper__wrapper"></div>
-          <img src="https://res.cloudinary.com/fe-advjs-final-project-3/image/upload/v1660230425/Banner__AWFAO_Hero_Desktop2_xd2mw5.webp" />
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="swiper__slider" onClick={() => console.log()}>
-        <Link to="/" className="">
-          <div className="swiper__wrapper"></div>
-          <img src="https://res.cloudinary.com/fe-advjs-final-project-3/image/upload/v1660230435/50__Sale_Desktop_scpavm.webp" />
-        </Link>
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        className="swiper"
+        modules={[Navigation, Pagination, Scrollbar]}
+        effect="slider"
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+        }}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+        navigation={true}
+        speed={1500}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {sliders.map((slide) => {
+          return (
+            <SwiperSlide key={slide._id} className="swiper__slider">
+              <Link to="/" className="">
+                <div className="swiper__wrapper"></div>
+                <img src={slide.imageUrl} />
+              </Link>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
   );
 };
 
