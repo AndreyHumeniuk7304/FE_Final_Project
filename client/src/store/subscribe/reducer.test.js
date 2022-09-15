223adf0fdb3b6e89538c1ebf19fed6e8c7a6a0dd
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-09-15 22:57:47 +0300

    upd test

--- client/src/store/subscribe/reducer.test.js
+++ client/src/store/subscribe/reducer.test.js
@@ -7,12 +7,14 @@ describe("subscribe reducer", () => {
         {
           isSubscribe: false,
           email: null,
+          error: {},
         },
         {}
       )
     ).toEqual({
       isSubscribe: false,
       email: null,
+      error: {},
     });
   });
 
@@ -22,15 +24,17 @@ describe("subscribe reducer", () => {
         {
           isSubscribe: true,
           email: "test@gmail.com",
+          error: {},
         },
         {
           type: "GET_SUBSCRIBE",
-          payload: { isSubscribe: true, email: "test@gmail.com" },
+          payload: { enabled: true, email: "test@gmail.com" },
         }
       )
     ).toEqual({
       isSubscribe: true,
       email: "test@gmail.com",
+      error: {},
     });
   });
   it("should to dellete subscribe", () => {
@@ -39,6 +43,7 @@ describe("subscribe reducer", () => {
         {
           isSubscribe: false,
           email: null,
+          error: {},
         },
         {
           type: "DELETE_SUBSCRIBE",
@@ -48,6 +53,26 @@ describe("subscribe reducer", () => {
     ).toEqual({
       isSubscribe: false,
       email: null,
+      error: {},
+    });
+  });
+  it("should to set error", () => {
+    expect(
+      reducer(
+        {
+          isSubscribe: false,
+          email: null,
+          error: { message: "error" },
+        },
+        {
+          type: "SET_SUBSCRIBE_ERROR",
+          payload: { message: "error" },
+        }
+      )
+    ).toEqual({
+      isSubscribe: false,
+      email: null,
+      error: { message: "error" },
     });
   });
 });
