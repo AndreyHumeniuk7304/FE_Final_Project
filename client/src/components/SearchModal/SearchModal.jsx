eaf2dbe8da4f92ce6d1355c655842836cfb75833
Bogdan <litvinenkobv99@gmail.com>
2022-09-21 21:20:28 +0300

    remove scss, add material ui

--- client/src/components/SearchModal/SearchModal.jsx
+++ client/src/components/SearchModal/SearchModal.jsx
@@ -1,90 +1,114 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import getFilterProducts from "../../api/getFilterProducts";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import { useDispatch } from "react-redux";
 import { writeSearchWord } from "../../store/catalog/actions";
+import { getFoundProducts } from "../../api/searchProduct";
+import { Box, Button } from "@mui/material";
+import theme from "../../theme";
 
 const SearchModal = ({ value, activeFocus, setActiveFocus, clearInput }) => {
   const dispatch = useDispatch();
-  let count = 0;
   const [foundProducts, setFoundProducts] = useState([]);
 
   useEffect(() => {
-    getFilterProducts("products/search", {
-      query: value.toLowerCase(),
-    }).then((products) => {
+    getFoundProducts(value).then((products) => {
       setFoundProducts(products);
       products.length >= 1 && setActiveFocus(true);
     });
   }, [value]);
 
-  // useEffect(() => {
-  //   localStorage.setItem("searchWorld", JSON.stringify(value));
-  // }, [foundProducts]);
+  const getProducts = () => {
+    return foundProducts.map((product, index) => {
+      if (
+        product.name.toLowerCase().includes(value.toLowerCase()) &&
+        index <= 3
+      ) {
+        return (
+          <Link
+            to={product.productUrl}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px",
+              width: "320px",
+              borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+            }}
+            key={product._id}
+            onClick={() => clearInput()}
+          >
+            <Box sx={{ flex: "0 1 30%" }}>
+              <Box
+                sx={{ width: "100%" }}
+                component={"img"}
+                src={product.imageUrls[0]}
+              />
+            </Box>
+            <Box sx={{ flex: "0 1 70%" }}>
+              <Box sx={{ padding: "0 0 20px 0", color: "black" }}>
+                {product.name}
+              </Box>
+
+              <Box sx={{ color: "black" }}>
+                <Box
+                  sx={{ display: "inline", color: "black", fontWeight: "bold" }}
+                >
+                  {product.currentPrice}
+                </Box>
+                $
+              </Box>
+            </Box>
+          </Link>
+        );
+      }
+      if (index == 4) {
+        return (
+          <Box textAlign="center" key={"button"}>
+            <Button
+              variant="text"
+              sx={{
+                color: "#000",
+                margin: "10px auto",
+              }}
+            >
+              <Link
+                to={`/products/filter?categories=mens,ladies,accessories&brand=${value}`}
+                onClick={() => {
+                  clearInput();
+                  dispatch(writeSearchWord(`${value}`));
+                }}
+                style={{ color: "#000" }}
+              >
+                Show all results
+              </Link>
+            </Button>
+          </Box>
+        );
+      }
+    });
+  };
 
   return (
     <>
-      {activeFocus ? (
-        <div className="modal">
-          <div className="modal__content">
-            {foundProducts.map((product) => {
-              count++;
-              if (
-                product.name.toLowerCase().includes(value.toLowerCase()) &&
-                count <= 4
-              ) {
-                return (
-                  <Link
-                    to={product.productUrl}
-                    className="modal__product"
-                    key={product._id}
-                    onClick={() => clearInput()}
-                  >
-                    <div className="modal__product-img">
-                      <img src={product.imageUrls[0]} />
-                    </div>
-                    <div className="modal__product-wrapper">
-                      <div className="modal__product-name">{product.name}</div>
-
-                      <div className="modal__product-price">
-                        <span>{product.currentPrice}</span> $
-                      </div>
-                    </div>
-                  </Link>
-                );
-              }
-              if (count == 5) {
-                return (
-                  <Box textAlign="center" key={"button"}>
-                    <Button
-                      variant="text"
-                      sx={{
-                        color: "#000",
-                        margin: "10px auto",
-                      }}
-                    >
-                      <Link
-                        to={`/products/filter?categories=mens,ladies,accessories&brand=${value}`}
-                        onClick={() => {
-                          clearInput();
-                          dispatch(writeSearchWord(`${value}`));
-                        }}
-                        className="modal__btn"
-                      >
-                        Show all results
-                      </Link>
-                    </Button>
-                  </Box>
-                );
-              }
-            })}
-          </div>
-        </div>
-      ) : (
-        ""
+      {activeFocus && (
+        <Box
+          className="modal"
+          sx={{
+            backgroundColor: "#fff",
+            position: "fixed",
+            top: "80px",
+            left: "55%",
+            zIndex: 10,
+            border: "1px solid black",
+            borderRadius: "10px",
+            [theme.breakpoints.down("desktop")]: {
+              top: "140px",
+              left: " calc(50% - 160px)",
+            },
+          }}
+        >
+          <Box>{getProducts()}</Box>
+        </Box>
       )}
     </>
   );
