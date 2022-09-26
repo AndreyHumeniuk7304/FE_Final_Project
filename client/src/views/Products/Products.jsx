b9ade5381171c394f08f69dda7e441ad70c3fee4
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-09-26 13:04:12 +0300

    delete Catalog and use only Products view

--- client/src/views/Products/Products.jsx
+++ client/src/views/Products/Products.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
-import Catalog from "../../components/Catalog/Catalog";
 import { fetchCategoriesProducts } from "../../store/catalog/actions";
+import Filter from "../../components/Catalog/Filter/Filter";
+import ProductList from "../../components/Catalog/ProductList/ProductList";
+import PropTypes from "prop-types";
+import { Stack } from "@mui/material";
 
-const Products = () => {
+const Products = ({ isFilterShow = true, isToggleShow }) => {
   const dispatch = useDispatch();
   const location = useLocation();
   useEffect(() => {
@@ -12,9 +15,15 @@ const Products = () => {
   }, [location.search]);
 
   return (
-    <div>
-      <Catalog />
-    </div>
+    <Stack direction={{ mobile: "column", desktop: "row" }}>
+      {isFilterShow && <Filter />}
+      <ProductList isToggleShow={isToggleShow} />
+    </Stack>
   );
 };
 export default Products;
+
+Products.propTypes = {
+  isFilterShow: PropTypes.bool,
+  isToggleShow: PropTypes.bool,
+};
